cb65f4ed6e1408eaca3aac3eacf71c2f8471f147
khushal <totakhailkhushal@gmail.com>
2021-07-13 15:26:45 +0200

    just alert task done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,56 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Box = (/* ???? */) => {
+const Box = (props) => {
+  // console.log(props);
 
-  // ?????
-  
+  if (props.hide === true){
+    return null;
+  }
+
+  let type;
+  switch (props.type){
+    case "red":
+      type="danger"
+    break;
+    case"orange":
+      type= "warning";
+      break;
+    default:
+      type ="primary"
+  }
+
+ 
+    return( 
+    <div className={`alert alert-${type}`} role="alert">
+      {props.message}
+    </div>
+    );
+ 
 };
+
+
+
+
+
+// const Box = (props) => {
+//   return(
+//     <div className={`alert alert-${
+//       props.type === 'blue' ? "primary"
+//       :
+//       props.type === 'red' ? "danger"
+//       :
+//       props.type === 'orange' ? "warning" : null
+//     }`}>
+//         <p>
+//         {props.message}
+//         </p>
+//     </div>
+//   )};
+
+
+
+
 
 // Do not edit below this line
 const jsx = <>
